fbff8a76bd88b5efa77dee4fb776e4325bd3a274
ITALO <italo.ant.mg@gmail.com>
2026-02-26 15:26:46 -0300

    Initial commit

---
+++ plugins/timeline-block/includes/cool-timeline-block/dist/block.build.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b188336de3b8c39680f9');
---
+++ wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1767745432;s:7:"authKey";s:64:"w[kTTsi#DK9bgK_)IZ|cD P{0oIql/^EE9(N>]V7n=evOg!8bs]{$M~}0BHmt6)o";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:1;}
