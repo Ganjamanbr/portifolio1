82d9f7ff421613065f558d62d7fad63fdf94dfce
Ganjamanbr <italo.felisbino@estudante.iftm.edu.br>
2026-02-26 16:26:59 -0300

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,14 +1,3 @@
-<?php
-define('DB_NAME', $_ENV['MARIADB_DATABASE']);
-define('DB_USER', $_ENV['MARIADB_USER']);
-define('DB_PASSWORD', $_ENV['MARIADB_PASSWORD']);
-define('DB_HOST', $_ENV['MARIADB_HOST']);
-define('DB_PORT', $_ENV['MARIADB_PORT']);
-$table_prefix = 'wp_';
-define('WP_DEBUG', false);
-
-// Limites PHP
-ini_set('upload_max_filesize', '1024M');
-ini_set('post_max_size', '1024M');
-ini_set('memory_limit', '1024M');
-?>
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true);
+define('WP_DEBUG_DISPLAY', true);
