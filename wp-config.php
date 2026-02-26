5873ddb46e9071cebae55bed45475fa10f45030d
Ganjamanbr <italo.felisbino@estudante.iftm.edu.br>
2026-02-26 16:42:30 -0300

    Enable WordPress debugging features

--- wp-config.php
+++ wp-config.php
@@ -3,6 +3,10 @@ define('DB_NAME', $_ENV['DB_NAME']);
 define('DB_USER', $_ENV['DB_USER']);
 define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
 define('DB_HOST', $_ENV['DB_HOST']);
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true);
+define('WP_DEBUG_DISPLAY', true);
+@ini_set('display_errors', 1);
 $table_prefix = 'wp_';
 require_once ABSPATH . 'wp-settings.php';
 ?>
