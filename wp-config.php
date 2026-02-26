4fb9ba3a47574c836c8eeb3178ede09ef515db35
Ganjamanbr <italo.felisbino@estudante.iftm.edu.br>
2026-02-26 16:40:59 -0300

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,3 +1,8 @@
-define('WP_DEBUG', true);
-define('WP_DEBUG_LOG', true);
-define('WP_DEBUG_DISPLAY', true);
+<?php
+define('DB_NAME', $_ENV['DB_NAME']);
+define('DB_USER', $_ENV['DB_USER']);
+define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
+define('DB_HOST', $_ENV['DB_HOST']);
+$table_prefix = 'wp_';
+require_once ABSPATH . 'wp-settings.php';
+?>
